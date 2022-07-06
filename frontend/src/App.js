d39f060739bed73e2d60dd25a8bc1f6f6f6d619d
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-06 19:29:08 +0900

    create pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+// styles
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      tirso
+    <div>
+      App.js
     </div>
   );
 }
