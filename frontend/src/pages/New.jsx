7ff8b36b859e04c86a80cdefb378b27d539851c0
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-07 09:16:43 +0900

    start with new form

--- frontend/src/pages/New.jsx
+++ frontend/src/pages/New.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
 
 
 export default function New() {
+  const [name, setName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>New</div>
+    <div className=''>
+      <h2 className='page-title'>Add a new restaurant</h2>
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="name">Restaurant:</label>
+        <input type="text" id='name'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required />
+
+        <button
+          className='btn'
+        >Sumbit</button>
+
+      </form>
+    </div>
   )
 }
