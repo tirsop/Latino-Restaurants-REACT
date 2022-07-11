76f61dd43425fc4533678c8cb5673a14a2a97106
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-11 15:20:44 +0900

    moving the fetch logic to the home component

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,37 +1,18 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 // components
 import Restaurant from "./Restaurant";
 
-export default function List() {
-  const [restaurants, setRestaurants] = useState([]);
-  const [isPending, setIsPending] = useState(false);
-  // const [error, setError] = useState(false)
+export default function List({ restaurants }) {
 
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsPending(true);
-      const response = await fetch('http://localhost:3001/api/restaurants');
-      const data = await response.json();
-      setRestaurants(data.restaurants);
-      setIsPending(false);
-    };
-
-    fetchData();
-
-  }, []);
-
+  if (restaurants.length === 0) {
+    return <div className="error">No restaurants available at the moment. Why don't create one?</div>
+  }
 
   return (
-    <div className="">
-      {isPending && <p className='loading'>Loading.........</p>}
-      {restaurants && (
-        <div className="container">
-          <div className="row">
-            {restaurants.map(restaurant => <Restaurant key={restaurant._id} restaurant={restaurant} />)}
-          </div>
-        </div>
-      )}
+    <div className="container">
+      <div className="row">
+        {restaurants.map(restaurant => <Restaurant key={restaurant._id} restaurant={restaurant} />)}
+      </div>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+// import { useFetch } from "../hooks/useFetch";
 // components
 import Header from "../components/Header";
 import Map from "../components/Map";
@@ -5,11 +7,34 @@ import List from "../components/List";
 
 
 export default function Home() {
+  // // fetching data using a custom hook
+  // const { data: restaurants, isPending, error } = useFetch('http://localhost:3001/api/restaurants')
+
+  const [restaurants, setRestaurants] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+      const response = await fetch('http://localhost:3001/api/restaurants');
+      const data = await response.json();
+      setRestaurants(data.restaurants);
+      setIsPending(false);
+    };
+
+    fetchData();
+
+  }, []);
+
+
   return (
     <>
       <Header />
       <Map />
-      <List />
+      {/* {error && <p>{error}</p>} */}
+      {isPending && <p>Loading...</p>}
+      {restaurants && <List restaurants={restaurants} />}
     </>
   )
 }
