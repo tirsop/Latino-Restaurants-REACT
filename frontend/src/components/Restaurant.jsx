18c808eb5a9a2ba168d74d9ba3a8b776fa850b11
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-14 08:09:37 +0900

    add flag on restaurant card

--- frontend/src/components/Restaurant.jsx
+++ frontend/src/components/Restaurant.jsx
@@ -6,9 +6,14 @@ export default function Restaurant({ restaurant }) {
     <div className="col-xs-12 col-sm-6 col-md-4">
       <a href={restaurant.url} className="text-decoration-none" target="_blank" rel="noreferrer">
         <img src={restaurant.image} className="img-fluid rounded-2 restaurant-img" alt="Restaurant's front door" />
-        <div className="ps-1">
-          <h5 className="text-dark fs-5 fw-light mt-1 mb-0">{restaurant.name}</h5>
-          <p className="text-muted fs-6">{restaurant.location}</p>
+        <div className="d-flex justify-content-between">
+          <div className="ps-1">
+            <h5 className="text-dark fs-5 fw-light mt-1 mb-0">{restaurant.name}</h5>
+            <p className="text-muted fs-6">{restaurant.location}</p>
+          </div>
+          <div className="">
+            {restaurant.country === 'spain' && <div className="">🇪🇸</div>}
+          </div>
         </div>
       </a>
     </div>
