ace115c16dae738895499d4d75d8a70880eb4509
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-15 10:58:31 +0900

    style the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,17 +4,22 @@ import { Link } from 'react-router-dom'
 import './Header.css'
 
 export default function Header() {
+
   return (
-    <div className="d-flex justify-content-between align-items-start mb-3">
-      <div className="">
-        <h1>Spanish <span className="fw-light">Restaurants in Tokyo</span></h1>
-        <p className="fs-6 fw-light text-muted">Find where to eat authentic Spanish food in Tokyo</p>
+    <div className="header">
+      <div className="d-flex justify-content-between align-items-start mb-3">
+        <div>
+          <h1>Spanish <span className="fw-light">Food</span> <span className="span-domain">.jp</span></h1>
+          <p className="fs-6 fw-light text-muted">Find where to eat authentic latin food in Tokyo</p>
+          <div className="red-dot"></div>
+        </div>
+        <Link to='/restaurants/new' className="bg-light buttons button-add">
+          Add a restaurant
+        </Link>
       </div>
-      <Link to='/restaurants/new' className="bg-light buttons button-add">
-        Add a restaurant
-      </Link>
     </div>
   )
 }
 
- // todo: button occupies more space than it should be in small screen
+// todo: button occupies more space than it should be in small screen
+// todo: sticky header
