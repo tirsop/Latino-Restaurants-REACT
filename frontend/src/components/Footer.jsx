6ab541f0a99c72d48a880d1d2a95ab9123c0f0f7
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-16 09:31:51 +0900

    change index.html title

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css'
 export default function Footer() {
   return (
     <footer>
-      <p className="copyright">Spanish Restaurant Tokyo © 2022</p>
-      <p className="madeby">Site designed by:</p>
+      <p className="copyright">SpanishFood.jp © 2022 Tokyo</p>
+      <p className="madeby">Site created by:</p>
       <div className="tirsop">
         <a href="https://tirsop.com" target="_blank" rel="noreferrer">
           <img className="avatar" src="/img/TirsoPascual.jpeg" alt="Tirso Pascual" />
