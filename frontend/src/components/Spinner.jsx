206baef51726f9f90801c606d90a652b9db19853
Tirso Pascual <tpascualgarcia@gmail.com>
2022-07-21 20:53:26 +0900

    change style

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,9 +3,6 @@ import './Spinner.css'
 
 export default function Spinner() {
   return (
-    // <div className='loadingSpinnerContainer'>
-    //   <div className='loadingSpinner'></div>
-    // </div>
     <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
   )
 }
